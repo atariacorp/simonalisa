7e26ebee913799e0cddbc18bc1c5e037ac8d8a22
atariacorp <asthar.pramana@gmail.com>
2026-03-04 14:17:31 +0700

    Update SectionTitle.jsx

--- src/SectionTitle.jsx
+++ src/SectionTitle.jsx
@@ -1,4 +1,4 @@
-import SectionTitle from './SectionTitle';
+import React from 'react';
 
 const SectionTitle = ({ children }) => (
   <h2 className="text-2xl font-bold text-gray-800 dark:text-gray-200 mb-6">{children}</h2>
